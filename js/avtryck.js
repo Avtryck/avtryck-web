31dda9fd6edc93dc7230e7001115c90036ce9a7b
Johan Mjones <johan.mjones@gmail.com>
2011-03-19 10:37:37 +0100

    top bar

--- js/avtryck.js
+++ js/avtryck.js
@@ -1,8 +1,8 @@
 var avtryck = {
 	initMaps : function() {
-	    var latlng = new google.maps.LatLng( - 34.397, 150.644);
+	    var latlng = new google.maps.LatLng( 62.4007043202567, 17.2577392061653);
 	    var myOptions = {
-	        zoom: 8,
+	        zoom: 13,
 	        center: latlng,
 	        mapTypeId: google.maps.MapTypeId.ROADMAP
 	    };
